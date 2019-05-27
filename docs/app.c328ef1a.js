05b6e3214dde510f9bbae8393eba54228424a570
patmood <patrick.n.moody@gmail.com>
2019-05-27 15:03:22 -0700

    relative links for github pages

--- docs/app.c328ef1a.js
+++ docs/app.c328ef1a.js
@@ -216,7 +216,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55692" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56860" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -392,4 +392,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["node_modules/parcel/src/builtins/hmr-runtime.js","app.js"], null)
-//# sourceMappingURL=/app.c328ef1a.js.map
+//# sourceMappingURL=app.c328ef1a.js.map
